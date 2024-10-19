9b3fd0173850c8c8a915eb40bc3600ad3c1fc911
tapadar13 <monsurismyname@gmail.com>
2024-10-19 16:42:36 +0530

    integrate ExpenseList into main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import ExpenseList from "./components/expenses/ExpenseList";
 import Layout from "./components/layout/Layout";
+import { ExpenseProvider } from "./context/ExpenseContext";
 
 export default function App() {
   return (
-    <Layout>
-      <main>Content</main>
-    </Layout>
+    <ExpenseProvider>
+      <Layout>
+        <ExpenseList />
+      </Layout>
+    </ExpenseProvider>
   );
 }
